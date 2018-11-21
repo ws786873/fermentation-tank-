1ff3d4186c1a3d4efdb9df5ded8e1e9ce30b6232
ws786873 <ws786873@qq.com>
2018-11-21 12:30:07 +0800

    Add files via upload
    use dip to control hum,you need to change ki kd ...to make it work well for you.

---
+++ Hum_pid.ino
@@ -0,0 +1,141 @@
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// Depends on the following Arduino libraries:
+// - Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <PID_v1.h>
+
+//#define PIN_INPUT 0
+#define RELAY_PIN 5
+#define DHTPIN 14         // Pin which is connected to the DHT sensor.
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;              //PID
+
+//Specify the links and initial tuning parameters
+double Kp=1000, Ki=0, Kd=0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);//REVERSE
+
+int WindowSize = 5000;
+unsigned long windowStartTime;              //PID
+
+
+// Uncomment the type of sensor in use:
+//#define DHTTYPE           DHT11     // DHT 11 
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  windowStartTime = millis();             //PID
+
+  //initialize the variables we're linked to
+   Setpoint = 60;
+
+  //tell the PID to range between 0 and the full window size
+  myPID.SetOutputLimits(0, WindowSize);
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+
+
+
+  
+  Serial.begin(9600);   //DHT22
+  // Initialize device.
+  dht.begin();
+  Serial.println("DHTxx Unified Sensor Example");
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
+  Serial.println("------------------------------------");
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+  // Let's see what this PID loop is *really doing:
+
+void loop() {
+  sensors_event_t event;  
+  Input = (event.relative_humidity);
+  //Serial.println(Input);
+  //Serial.println(Setpoint);
+  
+  myPID.Compute();
+  
+  //Serial.println(Output);
+
+  /************************************************
+   * turn the output pin on/off based on pid output
+   ************************************************/
+  if (millis() - windowStartTime > WindowSize)
+  { //time to shift the Relay Window
+    windowStartTime += WindowSize;
+  }
+  
+  
+  if (Output < millis() - windowStartTime) digitalWrite(RELAY_PIN, LOW);
+  else digitalWrite(RELAY_PIN, HIGH);
+  //digitalWrite(RELAY_PIN, HIGH);
+
+
+  
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+ 
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+   // Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    //Serial.println(" *C");
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.println(event.relative_humidity);
+    Serial.println("%");
+  }
+
+
+ 
+}
